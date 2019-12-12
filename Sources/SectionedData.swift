7712adc9eefdb5790c50dac9396b7022dad6f394
Ivan Michaylovsky <ivan.michaylovsky@sequenia.com>
2019-12-12 16:52:46 +0700

    add modifying method

--- Sources/SectionedData.swift
+++ Sources/SectionedData.swift
@@ -13,23 +13,23 @@ public protocol ExtDifferentiable: Differentiable {
 }
 
 public extension ExtDifferentiable {
-
+    
     public static func == (lhs: Self, rhs: Self) -> Bool {
         return lhs.raw() == rhs.raw()
     }
-
+    
     public static func < (lhs: Self, rhs: Self) -> Bool {
         return lhs.raw() < rhs.raw()
     }
-
+    
 }
 
 public class SectionedData<SectionEnum: ExtDifferentiable> {
-
+    
     public init() {
         
     }
-
+    
     public private(set) var items = [ArraySection<SectionEnum, BaseTableCellData>]() {
         didSet {
             self.items.sort { (lhs, rhs) -> Bool in
@@ -37,17 +37,17 @@ public class SectionedData<SectionEnum: ExtDifferentiable> {
             }
         }
     }
-
+    
     // MARK: - Data Source
-
+    
     public func numberOfSections() -> Int {
         return self.items.count
     }
-
+    
     public func numberOfItemsIn(_ section: Int) -> Int {
         return self.items[section].elements.count
     }
-
+    
     public func itemAt(_ indexPath: IndexPath) -> BaseTableCellData {
         return self.items[indexPath.section].elements[indexPath.row]
     }
@@ -61,53 +61,70 @@ public class SectionedData<SectionEnum: ExtDifferentiable> {
         }
         return items
     }
-
+    
     public func typeAt(_ indexPath: IndexPath) -> SectionEnum {
         return self.items[indexPath.section].model
     }
-
+    
     // MARK: - Modifying
-
+    
     public func clear() {
         self.items.removeAll()
     }
-
+    
     public func removeAllBy(_ section: SectionEnum) {
         self.items.removeAll { $0.model == section }
     }
-
+    
     public func hasItemsIn(_ section: SectionEnum) -> Bool {
         return self.items.filter { $0.model == section }.count > 0
     }
-
+    
     public func append(_ item: ArraySection<SectionEnum, BaseTableCellData>) {
         self.items.append(item)
     }
-
+    
+    public func replace(_ item: ArraySection<SectionEnum, BaseTableCellData>) {
+        self.removeAllBy(item.model)
+        self.append(item)
+    }
+    
+    public func replaceData(_ data: BaseTableCellData, section: SectionEnum) {
+        var items = [BaseTableCellData]()
+        self.items.forEach { (item) in
+            if item.model == section {
+                items.append(contentsOf: item.elements)
+            }
+        }
+        
+        var item = items.filter { $0.identifier == data.identifier }.first
+        item = data
+    }
+    
     public func reload(_ data: [ArraySection<SectionEnum, BaseTableCellData>]) {
+        self.items.removeAll()
         self.items = data
     }
-
 }
 
 open class BaseTableCellData: Differentiable {
-
+    
     public var identifier: Int
-
+    
     public init(_ identifier: Int) {
         self.identifier = identifier
     }
-
+    
     public var differenceIdentifier: Int {
         return self.identifier
     }
-
+    
     open func isContentEqual(to source: BaseTableCellData) -> Bool {
         return self == source
     }
-
+    
     static func == (lhs: BaseTableCellData, rhs: BaseTableCellData) -> Bool {
         return lhs.identifier == rhs.identifier
     }
-
+    
 }
